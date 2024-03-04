74ba81990fc23095b6428899ff899ddfe8596dac
rodrigoo1604 <rodrigo.lastra.pescador@gmail.com>
2024-03-04 18:49:08 +0100

    UserContext implementation

--- front/src/pages/index.js
+++ front/src/pages/index.js
@@ -1,13 +1,14 @@
 import UserLoginPage from "./UserLoginPage";
+import UserGlobalState from "../context/UserGlobalState";
 
 
 export default function Home() {
   return (
-    <>
+    <UserGlobalState>
         <div>
       <h1>Welcome to BestSellet orders management dashboard!</h1>
         <UserLoginPage />
         </div>
-    </>
+    </UserGlobalState>
   );
 }
