f7a1618c4e0df6b4a664d4726a4e321175b69eb5
rodrigoo1604 <rodrigo.lastra.pescador@gmail.com>
2024-03-04 18:51:41 +0100

    globalstate fix in index

--- front/src/pages/index.js
+++ front/src/pages/index.js
@@ -1,5 +1,5 @@
 import UserLoginPage from "./UserLoginPage";
-import UserGlobalState from "../context/UserGlobalState";
+import UserGlobalState from "../core/GlobalState";
 
 
 export default function Home() {
