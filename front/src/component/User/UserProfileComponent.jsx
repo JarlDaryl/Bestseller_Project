967c96147ef29cbaf72c063e532e41285af964dd
rodrigoo1604 <rodrigo.lastra.pescador@gmail.com>
2024-04-03 18:30:39 +0200

    user profile styles

--- front/src/component/User/UserProfileComponent.jsx
+++ front/src/component/User/UserProfileComponent.jsx
@@ -1,71 +1,80 @@
 import React, { useState, useEffect } from 'react';
 import { changeEmail } from '../../api/UsersAPIFetch';
 import CircularProgress from '@mui/material/CircularProgress';
-import HeaderMenuBarOrdersComponent from '../HeaderMenuBar/HeaderMenuBarOrderComponent';
 import Button from '@mui/material/Button';
 import ArrowBack from '@mui/icons-material/ArrowBack';
-import { useRouter } from 'next/router';
+import {useRouter} from 'next/router';
 
 export default function UserProfileComponent() {
 	const [userDetails, setUserDetails] = useState(null);
 	const [newEmail, setNewEmail] = useState('');
-	const router = useRouter();
+  const router = useRouter();
 
 	useEffect(() => {
-		const user = window.sessionStorage.getItem('user');
-		if (user) {
-			const userObj = JSON.parse(user);
-			setUserDetails(userObj.data);
-		}
-		console.log(user)
+    const user = window.sessionStorage.getItem('user');
+    if (user) {
+        const userObj = JSON.parse(user);
+        setUserDetails(userObj.data); 
+    }
+    console.log(user)
 
-	}, []);
+}, []);
 
 	const handleEmailChange = async () => {
-		const user = JSON.parse(window.sessionStorage.getItem('user'));
-		if (user && user.data) {
-			const userId = user.data.id;
-			await changeEmail(newEmail, userId);
-		} else {
-			console.error('User not found in session storage');
-		}
-	};
+    const user = JSON.parse(window.sessionStorage.getItem('user'));
+    if (user && user.data) { 
+        const userId = user.data.id; 
+        await changeEmail(newEmail, userId);
+    } else {
+        console.error('User not found in session storage');
+    }
+};
 
 	if (!userDetails) {
 		return <CircularProgress />;
 	}
 
-	const handleBackClick = () => {
-		router.push('/DashboardPage');
-	};
+  const handleBackClick = () => {
+    router.push('/DashboardPage'); // Redirect to dashboard
+};
 
 	return (
-		<div>
-			<HeaderMenuBarOrdersComponent />
-			<h1>User Profile</h1>
-			<p>
-				{' '}
-				Company: <strong> {userDetails.companyName}</strong>
-			</p>
-			<p>
-				{' '}
-				Country: <strong> {userDetails.country}</strong>
-			</p>
-			<p>
-				{' '}
-				Current email: <strong> {userDetails.email}</strong>
-			</p>
-
-			<input
-				type='email'
-				value={newEmail}
-				onChange={(e) => setNewEmail(e.target.value)}
-				placeholder='Enter new email'
-			/>
-			<button onClick={handleEmailChange}>Change Email</button>
+		<div className='profile-display'>
+			<h2 className='profile-h2'>User Profile</h2>
 			<div>
-				<Button onClick={handleBackClick} startIcon={<ArrowBack />}>Dashboard</Button>
+				<p className='profile-title'>
+					{' '}
+					Company: <strong> {userDetails.companyName}</strong>
+				</p>
 			</div>
+				<div>
+				<p className='profile-title'>
+					{' '}
+					Country: <strong> {userDetails.country}</strong>
+				</p>
+			</div>
+			<div>
+				<p className='profile-title'>
+					{' '}
+					Current email: <strong> {userDetails.email}</strong>
+				</p>
+			</div>
+			<div>
+				<p className='new-email-title'>
+					New email
+				</p>
+				<div>
+					<input
+						type='email'
+						value={newEmail}
+						onChange={(e) => setNewEmail(e.target.value)}
+						className='profile-input'
+					/></div>
+				</div>
+			<div>
+				<button onClick={handleEmailChange} className='profile-button'>Change Email</button>
+			</div>
+     		<Button onClick={handleBackClick} startIcon={<ArrowBack />} className='profile-return-button'>Return</Button> 
 		</div>
 	);
 }
--- front/src/pages/UserProfilePage.jsx
+++ front/src/pages/UserProfilePage.jsx
@@ -1,10 +1,12 @@
 import UserProfileComponent from '../component/User/UserProfileComponent'
+import HeaderMenuBarOrdersComponent from '@/component/HeaderMenuBar/HeaderMenuBarOrderComponent'
 import React from 'react'
 
 export default function UserProfilePage() {
   return (
     <div>
-        <UserProfileComponent/>
+      <HeaderMenuBarOrdersComponent />
+      <div className='profile-page-container'><UserProfileComponent/></div>
     </div>
   )
 }
